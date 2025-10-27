6cf55bbb91c095028043bc3f4fa1f259a17b8ebb
emilyma214-alt <emilyma214@berkeley.edu>
2025-10-27 16:43:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib21paTEyMyIsImEiOiJjbWg5cjJiZXowcDhpMmtwdmU4N2I4Z3NqIn0.OLgjMvZXtL4emMs9Uzo6zw';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  center: [-122.2727, 37.8715], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  style: 'mapbox://styles/omii123/cmh9rfzbh00bl01sq2ktreulg',
+  zoom: 9 // starting zoom
+    });
