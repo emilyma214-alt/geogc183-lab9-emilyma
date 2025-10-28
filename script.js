428340ca329caf76b8452cdeddddc730d6b39a8a
emilyma214-alt <emilyma214@berkeley.edu>
2025-10-27 17:03:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const map = new mapboxgl.Map({
   container: 'map', // container ID
   center: [-122.2727, 37.8715], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   style: 'mapbox://styles/omii123/cmh9rfzbh00bl01sq2ktreulg',
-  zoom: 9 // starting zoom
+  zoom: 12 // starting zoom
     });
